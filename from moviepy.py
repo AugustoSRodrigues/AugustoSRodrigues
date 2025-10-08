6d93ac7438f28c04b00c1728376c6c04eb9cf023
AugustoSRodrigues <augustorodrigues1@OUTLOOK.com>
2025-10-08 15:19:52 -0300

    heimdall

--- from moviepy.py
+++ from moviepy.py
@@ -2,7 +2,7 @@ from moviepy import VideoFileClip
 
 def mp4_para_gif(input_mp4, output_gif, fps=10):
     clip = VideoFileClip(input_mp4)
-    clip.duration = min(clip.duration, 5)  # Limita a duração a 10 segundos
+    clip.duration = min(clip.duration, 2)  # Limita a duração a 10 segundos
     clip.write_gif(output_gif, fps=fps)
     print(f"GIF salvo em: {output_gif}")
 
